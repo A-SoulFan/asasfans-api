8270b96e57dd8a31110b699c6482361b2e2eb6c5
runstp <runstp@163.com>
2022-04-15 01:43:21 +0800

    :art: 调整爬虫skip相关逻辑

--- internal/app/asasapi/spider/tool.go
+++ internal/app/asasapi/spider/tool.go
@@ -6,6 +6,10 @@ import (
 	"github.com/A-SoulFan/asasfans-api/internal/pkg/bilibili"
 )
 
+const (
+	allowTags = "嘉然,向晚,贝拉,珈乐,乃琳,阿草,嘉心糖,嘉然今天吃什么,A-SOUL,a-soul,传说的世界,向晚大魔王,顶晚人,乃琳Queen,乃淇琳,贝拉kira,贝极星,珈乐Carol,ASOUL,asoul,GNK48,超级敏感,传说的世界,A-SOUL二创激励计划,乃贝,嘉晚饭,果丹皮,琳嘉"
+)
+
 func calculateScore(info *bilibili.VideoInfoResponse) uint64 {
 	score := float64(info.Stat.View)*0.25 +
 		float64(info.Stat.Like+info.Stat.Coin+info.Stat.Reply+info.Stat.Like)*0.4 +
@@ -19,8 +23,10 @@ func isSkip(sInfo bilibili.VideoSearchInfo, keyword string) bool {
 	tags := strings.Split(sInfo.Tag, ",")
 	// 防止错误的收录不属于 keyword 的内容
 	for _, tag := range tags {
-		if tag == keyword {
-			return false
+		for _, allowTag := range strings.Split(allowTags, ",") {
+			if tag == allowTag || tag == keyword {
+				return false
+			}
 		}
 	}
 
